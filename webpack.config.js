b42ef4bd70ff009da9888376bd2ba7eec780b482
Dawid Urbański <hi@dawidurbanski.com>
2019-05-30 03:20:48 +0200

    Cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -50,10 +50,7 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
               publicPath: path.resolve(__dirname, 'dist'),
-              hmr: process.env.NODE_ENV === 'development',
             },
           },
           'css-loader',
